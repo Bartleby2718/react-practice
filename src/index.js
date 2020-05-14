a31788410ad73a44c90ba06f843d196df83db319
Jihoon Park <bartleby2718@gmail.com>
2020-05-14 09:36:05 +0900

    Show an alert upon clicking Square

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => alert("click")}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
